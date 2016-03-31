ff043dfe411f214f13e6941c1eb9934a7f930de8
Richard Premvaree <richard.premvaree@gmail.com>
2016-03-31 17:19:49 +0000

    sinatra tutorial

--- application_controller.rb
+++ application_controller.rb
@@ -6,5 +6,29 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+  get '/other_side' do
+    erb :index
+  end
+  
+  post '/other_side' do
+    erb :other_side
+  end
 
 end
+
+
+
+
+
+
+
+
+  
+#   get '/other_side' do
+#     erb :index
+#   end
+  
+#   post '/other_side' do
+#     erb :other_side
+#   end
